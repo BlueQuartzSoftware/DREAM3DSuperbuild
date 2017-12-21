35b13d5bb50a138734509d72f627cefddd12dbaf
Michael Jackson <mike.jackson@bluequartz.net>
2017-12-21 12:17:11 -0500

    Adding provision to use Qt 5.9.3 or 5.10.0

--- cmake/External_Qt5.cmake
+++ cmake/External_Qt5.cmake
@@ -1,14 +1,23 @@
-set(extProjectName "Qt")
+
+set(Qt510 "1")
+# Qt 5.x is going to be used.
+if(Qt510)
+  set(qt5_version_major "5.10")
+  set(qt5_version_full "5.10.0")
+  set(qt5_version_short "5.10.0")
+  # This variable is used inside the javascript file that performs the Qt installation
+  set(qt5_installer_version "qt5.5100")
+else()
+  set(qt5_version_major "5.9")
+  set(qt5_version_full "5.9.3")
+  set(qt5_version_short "5.9.3")
+  # This variable is used inside the javascript file that performs the Qt installation
+  set(qt5_installer_version "593")
+endif()
+
+set(extProjectName "Qt${qt5_version_full}")
 message(STATUS "External Project: ${extProjectName}" )
 
-# set(qt5_version_full "5.6.2")
-# set(qt5_version_short "5.6")
-# set(qt5_installer_version "56")
-
-# Qt 5.9.x is going to be used.
-set(qt5_version_full "5.9.3")
-set(qt5_version_short "5.9.3")
-set(qt5_installer_version "593")
 
 
 set(qt5_INSTALL "${DREAM3D_SDK}/${extProjectName}${qt5_version_full}")
@@ -16,7 +25,7 @@ set(qt5_BINARY_DIR "${DREAM3D_SDK}/superbuild/${extProjectName}/Build")
 
 get_filename_component(_self_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
 
-set(QT_INSTALL_LOCATION "${DREAM3D_SDK}/${extProjectName}${qt5_version_full}")
+set(QT_INSTALL_LOCATION "${DREAM3D_SDK}/${extProjectName}")
 
 if(APPLE)
   set(qt5_Headless_FILE "Qt_HeadlessInstall_OSX.js")
@@ -33,7 +42,7 @@ endif()
 if(MSVC14)
   set(QT_MSVC_VERSION_NAME "msvc2015_64")
 endif()
-if(MSVC15)
+if(MSVC_VERSION GREATER 1900)
   set(QT_MSVC_VERSION_NAME "msvc2017_64")
 endif()
 
@@ -45,9 +54,7 @@ configure_file(
 )
 
 if(APPLE)
-  #set(qt5_url "http://qt.mirror.constant.com/archive/qt/${qt5_version_short}/${qt5_version_full}/qt-opensource-mac-x64-clang-${qt5_version_full}.dmg")
-
-  set(qt5_url "http://qt.mirror.constant.com/archive/qt/5.9/5.9.3/qt-opensource-mac-x64-5.9.3.dmg")
+  set(qt5_url "http://qt.mirror.constant.com/archive/qt/${qt5_version_major}/${qt5_version_short}/qt-opensource-mac-x64-${qt5_version_full}.dmg")
 
   set(Qt5_OSX_BASE_NAME qt-opensource-mac-x64-${qt5_version_full})
 
@@ -85,12 +92,12 @@ if(APPLE)
   set(QMAKE_EXECUTABLE ${QT_INSTALL_LOCATION}/${qt5_version_short}/clang_64/bin/qmake)
 
 elseif(WIN32)
-  set(qt5_online_installer "qt-unified-windows-x86-2.0.3-online.exe")
-  set(qt5_url "https://download.qt.io/archive/online_installers/2.0/${qt5_online_installer}")
+  set(qt5_online_installer "qt-opensource-windows-x86-${qt5_version_full}.exe")
+  set(qt5_url "http://qt.mirror.constant.com/archive/qt/${qt5_version_major}/${qt5_version_short}/qt-opensource-windows-x86-${qt5_version_full}.exe")
 
   if(NOT EXISTS "${DREAM3D_SDK}/superbuild/${extProjectName}/Download/${qt5_online_installer}")
     message(STATUS "===============================================================")
-    message(STATUS "   Downloading ${extProjectName}${qt5_version_full}")
+    message(STATUS "   Downloading ${extProjectName}")
     message(STATUS "   Large Download!! This can take a bit... Please be patient")
     file(DOWNLOAD ${qt5_url} "${DREAM3D_SDK}/superbuild/${extProjectName}/Download/${qt5_online_installer}" SHOW_PROGRESS)
   endif()
@@ -102,7 +109,7 @@ elseif(WIN32)
     @ONLY
   )
 
-  if(NOT EXISTS "${DREAM3D_SDK}/${extProjectName}${qt5_version_full}")
+  if(NOT EXISTS "${DREAM3D_SDK}/${extProjectName}")
     message(STATUS "Executing the Qt5 Installer... ")
     execute_process(COMMAND "${DREAM3D_SDK}/superbuild/${extProjectName}/Download/Qt_HeadlessInstall.bat"
                     OUTPUT_FILE "${DREAM3D_SDK}/superbuild/${extProjectName}/Download/qt-unified-out.log"
--- cmake/Qt_HeadlessInstall_OSX.js
+++ cmake/Qt_HeadlessInstall_OSX.js
@@ -28,10 +28,28 @@ Controller.prototype.TargetDirectoryPageCallback = function()
 Controller.prototype.ComponentSelectionPageCallback = function() {
     var widget = gui.currentPageWidget();
     
-    // This is for Qt 5.9.1 installation
+    // This is for Qt5 installation
     widget.deselectAll();
     widget.selectComponent("qt.@qt5_installer_version@.clang_64");
     widget.selectComponent("qt.@qt5_installer_version@.qtwebengine");
+
+    widget.selectComponent("qt.@qt5_installer_version@.doc");
+    widget.selectComponent("qt.@qt5_installer_version@.doc.qtcharts");
+    widget.selectComponent("qt.@qt5_installer_version@.doc.qtdatavis3d");
+    widget.selectComponent("qt.@qt5_installer_version@.doc.qtwebengine");
+    widget.selectComponent("qt.@qt5_installer_version@.examples");
+    widget.selectComponent("qt.@qt5_installer_version@.examples.qtcharts");
+    widget.selectComponent("qt.@qt5_installer_version@.examples.qtdatavis3d");
+    widget.selectComponent("qt.@qt5_installer_version@.examples.qtwebengine");
+    widget.selectComponent("qt.@qt5_installer_version@.qtcharts");
+    widget.selectComponent("qt.@qt5_installer_version@.qtcharts.clang_64");
+    widget.selectComponent("qt.@qt5_installer_version@.qtdatavis3d");
+    widget.selectComponent("qt.@qt5_installer_version@.qtdatavis3d.clang_64");
+    widget.selectComponent("qt.@qt5_installer_version@.qtwebengine");
+    widget.selectComponent("qt.@qt5_installer_version@.qtwebengine.clang_64");
+    widget.selectComponent("qt.@qt5_installer_version@.src");
+    widget.selectComponent("qt.@qt5_installer_version@.clang_64");
+    widget.selectComponent("qt.tools.qtcreator");
 
     gui.clickButton(buttons.NextButton);
 }
