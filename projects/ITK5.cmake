34ceecd04cbb180c8fb75130b7dfb2184ccd4c84
Dženan Zukić <dzenan.zukic@kitware.com>
2019-06-17 09:54:37 -0400

    Update version of ITKMontage module
    This includes the correct handling of spacing,
    exposing tile position tolerance, addition of
    global optimization with outlier detection and removal.

--- projects/ITK5.cmake
+++ projects/ITK5.cmake
@@ -110,7 +110,7 @@ ExternalProject_Add(${extProjectName}
     -DModule_ITKImageNoise=ON
 
     -DModule_Montage=ON
-    -DREMOTE_GIT_TAG_Montage:STRING=e34663554a95e483186c20be1ef4e794193bd1a1
+    -DREMOTE_GIT_TAG_Montage:STRING=1962de8ab6a00eb698a5a3716a9c6d5d04eb167d
   
   DEPENDS hdf5
   LOG_DOWNLOAD 1
